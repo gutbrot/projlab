0b998263e79e6c6ad541fce1a5ed91c745132c0d
gutbrot <gutbrod.adam@gmail.com>
2026-03-27 14:43:19 +0100

    maven beallitas proba
    mappa szerkezet letrehozasa
    pom fajl, teszt java fajl, github mappa

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("GitHub Actions teszt: SIKERES FORDITAS!");
+        
+        // Egy kis matek, hogy biztosan legyen mit fordítani
+        int a = 10;
+        int b = 20;
+        System.out.println("Osszeg: " + (a + b));
+    }
+}
