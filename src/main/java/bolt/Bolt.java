30f04bfdd00970bac4ce2fd46549362c151f26c2
gutbrot <gutbrod.adam@gmail.com>
2026-03-28 14:04:52 +0100

    Alaposztalyok letrehozasa, TODO lista hozzaadasa

---
+++ src/main/java/bolt/Bolt.java
@@ -0,0 +1,7 @@
+package bolt;
+
+import java.util.Map;
+
+public class Bolt {
+    private Map<String, Integer> termekek; // termék neve és ára
+}
---
+++ src/main/java/jarmu/Hokotro.java
@@ -0,0 +1,16 @@
+package jarmu;
+
+import kotrofej.KotroFej;
+import terkep.*;
+
+public class Hokotro extends Jarmu{
+    private KotroFej felszereltFej;
+    
+    @Override
+    public void mozgas(){
+    }
+
+    @Override
+    public void utkozos(){
+    }
+}
